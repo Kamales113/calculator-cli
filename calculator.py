a147b2fad106e81683854f1ff7cc140804919cc9
Kamales <kamales113@gmail.com>
2025-09-22 22:13:57 +0530

    Original code for calculator

---
+++ calculator.py
@@ -0,0 +1,33 @@
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a * b
+def div(a,b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+
+def calculate(num1,num2):
+     operation = input("The operation are +,-,*,/: ")
+     print("Welcom to the calculator")
+     print("the operation are +,-,*,/")
+     print("If you wan to exit type exit")
+     while True:
+        if operation =="exit":
+            break
+        num1 = float(input("Enter the first Number:"))
+        num2 = float(input("Enter the second Number:"))
+        if operation =="+":
+            print(add(num1,  num2))
+        elif operation == "-":
+            print(sub(num1,num2))
+        elif operation == "*":
+            print(mul(num1,num2))
+        elif operation == "/":
+            print(div(num1,num2))
+        else:
+            print("Invalid number")
+
+calculate()
